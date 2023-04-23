46e417e9607b725460b7f04323ba88dbfe1c2eb9
Yatin Kalra <yatin.kalra22@gmail.com>
2023-04-23 16:08:57 +0530

    matching subsequence based on multi pointer

---
+++ ds/multi-pointer/array/unsorted/string/matchSubsequence.js
@@ -0,0 +1,27 @@
+function matchSubsequence(subStr, str) {
+  if (!subStr[0] || !str[0]) return false;
+  if (subStr.length > str.length) return false;
+  if (str.length === 1 && str.length) {
+    if (str === subStr) return true;
+    return false;
+  }
+
+  let left = 0;
+  if (str[left] === subStr[left]) left += 1;
+  for (let right = 1; right < str.length; right++) {
+    if (left === subStr.length - 1) return true;
+    if (str[right] === subStr[left]) {
+      left++;
+    }
+  }
+
+  return false;
+}
+console.log(matchSubsequence("abc", "lmabracadabra")); // true
+console.log(matchSubsequence("abc", "abracadabra")); // true
+console.log(matchSubsequence("abc", "acb")); //false (order matters)
+console.log(matchSubsequence("1", "1")); // true
+console.log(matchSubsequence("", "")); // false
+console.log(matchSubsequence("1", "")); // false
+console.log(matchSubsequence("", "1")); // false
+console.log(matchSubsequence("abc", "a")); // false
